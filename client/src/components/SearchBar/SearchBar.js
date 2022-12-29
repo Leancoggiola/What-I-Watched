939f4c62b6435c43b930e0ca96ae97101b3ea81a
Leandro Coggiola <leandrocoggiola@hotmail.com>
2022-12-29 18:26:16 -0300

    Changed CRA to Webpack

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,3 @@
-// Assets
-import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg'
 // Styling
 import './SearchBar.scss'
 
@@ -7,7 +5,7 @@ const SearchBar = () => {
     return (
         <div class='search-bar'>
             <input type='text' placeholder='Busca series y peliculas..'></input>
-            <button type='submit'><SearchIcon/></button>
+            <button type='submit'></button>
         </div>
     )
 }
