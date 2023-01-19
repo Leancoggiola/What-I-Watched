bb819b4b9efff0de0f2553b990cbceda9fc098fa
Leandro Coggiola <leandrocoggiola@hotmail.com>
2023-01-18 22:49:39 -0300

    Logo and reworked process env front

--- client/src/index.js
+++ client/src/index.js
@@ -9,14 +9,16 @@ import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
+const getConfiguration = () => ({
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  redirectUri:window.location.origin,
+  cacheLocation: process.env.REACT_APP_AUTH0_CACHE_LOCATION
+})
+
 root.render(
   <Provider store={store}>
-    <Auth0Provider 
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri= {window.location.origin}
-      cacheLocation={process.env.REACT_APP_AUTH0_CACHE_LOCATION}
-      >
+    <Auth0Provider {...getConfiguration()}>
       <App/>
     </Auth0Provider>
   </Provider>
--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
     return (
         <div className='login-container'>
             <div className='login-card'>
-                <h1>Bienvenido a What I Saw</h1>
+                <h1>Bienvenido a What I Watched</h1>
                 <p>Una plataforma para hacer seguimiento de peliculas y series</p>
                 <button type='submit' className='btn btn-primary' onClick={() => window.location.href = '/'}>Login</button>
             </div>
