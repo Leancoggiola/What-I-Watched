63adee3f4ae96651731cbd77f5f39867be57002a
Leandro Coggiola <leandrocoggiola@hotmail.com>
2023-01-19 16:22:15 -0300

    reordered

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { Auth0Provider } from '@auth0/auth0-react';
 import store from './middleware/store.js';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if(process.env.NODE_ENV === 'production') {
-  disableReactDevTools()
-}
 
 // App Component
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
+
+if(process.env.NODE_ENV === 'production') {
+  disableReactDevTools()
+}
 
 const getConfiguration = () => ({
   domain: process.env.REACT_APP_AUTH0_DOMAIN,
